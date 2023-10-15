9be0e8c84bb4cf463be55981b1b46dc36f2b9059
Joey Fitzpatrick <joeyf116@yahoo.com>
2023-10-15 03:46:57 -0400

    test

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const openaiRoute = require("./routes/openaiRoute");
 const bodyParser = require("body-parser");
 const mongoDBConnString = process.env.MONGODB_CONN_STRING;
 const port = process.env.PORT || 5000;
-const path = require('path');
+const path = require("path");
 
 mongoose.connect(mongoDBConnString);
 const database = mongoose.connection;
@@ -24,20 +24,26 @@ const app = express();
 // Setting up basic middleware for all Express requests
 app.use(bodyParser.urlencoded({ extended: false })); // Parses urlencoded bodies
 app.use(bodyParser.json()); // Send JSON responses
+// middleware
+const corsOptions = {
+	origin: "localhost:3000", // frontend URI (ReactJS)
+};
+app.use(express.json());
+app.use(cors(corsOptions));
 app.use(cors());
 app.use(express.json());
 app.use("/api/transcript", transcriptRoute);
 app.use("/api/openai", openaiRoute);
 
-if (process.env.NODE_ENV === "production") {
-	// Serve the React Vite build files
-	app.use(express.static(path.join(__dirname, "../client/dist")));
+// if (process.env.NODE_ENV === "production") {
+// 	// Serve the React Vite build files
+// 	app.use(express.static(path.join(__dirname, "../client/dist")));
 
-	// For any other routes, serve the React app's HTML file
-	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-	});
-}
+// 	// For any other routes, serve the React app's HTML file
+// 	app.get("*", (req, res) => {
+// 		res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+// 	});
+// }
 
 app.listen(port, () => {
 	console.log(`Server Started a1t ${port}`);
